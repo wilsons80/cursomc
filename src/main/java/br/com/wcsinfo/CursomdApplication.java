5e4e1d19a47163bf39aa027955bf814914e3a5fc
Wilson Souza <wilsons80@gmail.com>
2020-01-14 18:57:16 -0300

    Versão inicial

---
+++ src/main/java/br/com/wcsinfo/CursomdApplication.java
@@ -0,0 +1,13 @@
+package br.com.wcsinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wcsinfo/CursomdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wcsinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
